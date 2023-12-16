c8520e2c8f39026574744e83f1d4c7d871f00789
Abhay <abhaycht256@gmail.com>
2023-12-16 20:40:49 +0530

    added Create and Edit

--- Trainer/Controllers/TamerController.cs
+++ Trainer/Controllers/TamerController.cs
@@ -16,5 +16,38 @@ namespace Trainer.Controllers
             List<Tamer> objTamerList = _db.Tamers.ToList(); 
             return View(objTamerList);
         }
-    }
+        
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+		public IActionResult Create(Tamer obj)
+		{
+            if(ModelState.IsValid)
+            {
+				_db.Tamers.Add(obj);
+				_db.SaveChanges();
+				
+			}
+			return RedirectToAction("Index");
+		}
+
+        public IActionResult Edit(int? Id)
+        {
+            if(Id  == null || Id== 0)
+            {
+                return NotFound();
+            }
+
+            Tamer? tamerFromDb = _db.Tamers.FirstOrDefault(t => t.Id == Id);
+            if(tamerFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(tamerFromDb);
+        }
+
+	}
 }
--- Trainer/Models/Tamer.cs
+++ Trainer/Models/Tamer.cs
@@ -1,13 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Trainer.Models
 {
     public class Tamer
     {
         [Key]
+        [DisplayName("ID")]
         public int Id { get; set; }
         [Required]
+        [MaxLength(100)] //Validation
         public string Name { get; set; }
-        public string Country { get; set; }
+		[MaxLength(100)] //Validation
+		public string Country { get; set; }
     }
 }
