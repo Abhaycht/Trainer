5c0d7cfb3d5da350f856b405d1bcf7af12011625
Abhay <abhaycht256@gmail.com>
2023-12-18 15:15:58 +0530

    Tamer CRUD and toastr notifications

--- Trainer/Controllers/TamerController.cs
+++ Trainer/Controllers/TamerController.cs
@@ -29,15 +29,18 @@ namespace Trainer.Controllers
             {
 				_db.Tamers.Add(obj);
 				_db.SaveChanges();
-				
+                TempData["success"] = "Category successfully";
+                return RedirectToAction("Index");
+
 			}
-			return RedirectToAction("Index");
+            return View();
 		}
 
         public IActionResult Edit(int? Id)
         {
             if(Id  == null || Id== 0)
             {
+                TempData["error"] = "error";
                 return NotFound();
             }
 
@@ -49,5 +52,48 @@ namespace Trainer.Controllers
             return View(tamerFromDb);
         }
 
+        [HttpPost]
+		public IActionResult Edit(Tamer obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Tamers.Update(obj);
+				_db.SaveChanges();
+                TempData["success"] = "Updated successfully";
+                return RedirectToAction("Index");
+
+			}
+			return View();
+
+		}
+
+		public IActionResult Delete(int? Id)
+		{
+			if (Id == null || Id == 0)
+			{
+                TempData["error"] = "error";
+                return NotFound();
+			}
+
+			Tamer? tamerFromDb = _db.Tamers.FirstOrDefault(t => t.Id == Id);
+			if (tamerFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(tamerFromDb);
+		}
+
+        [HttpPost]
+		public IActionResult Delete(Tamer obj)
+		{
+            Tamer? deleteObj = _db.Tamers.Find(obj.Id);
+            if (deleteObj == null)
+            {
+                return NotFound();
+            }
+            _db.Tamers.Remove(deleteObj);
+            _db.SaveChanges();
+			return RedirectToAction("Index");
+		}
 	}
 }
